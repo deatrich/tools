7c744cf3558f6885bf715248f046dcb3691da3f8
Denice Deatrich <denice.deatrich@gmail.com>
2023-06-05 15:57:30 -0600

    - change function names so they begin with 'f_'
    - use more understandable script arguments
    
    - add option to backup large directories
    
    - add opton to get a copy of a specified set of large directory backups

--- system-backup.sh
+++ system-backup.sh
@@ -3,27 +3,43 @@
 #
 # PURPOSE:	This is an example script for Linux system backups with
 #		lots of error checking.
-#		Note that we are backing-up files locally.  The best solution
-#		is to then copy backed-up files to removable media, or
-#		securely copy them to another device.
-#
+#		
 # AUTHOR:	D. Deatrich
 #
 # RETURNS:	0  - OK
 #		1  - an error occured .. see the script output
 #
-# USAGE:	/path/to/system-backup.sh [ -y ]
-# LOGIC:	Cycle through the days of the week, overwriting older backups
-#		in what is known as a circular buffer.
-#		As well, if you want to keep a year's worth of backups, but
-#		effectively keep the last week of each month, then the buffer
-#		is larger, but not overly expensive.
-#		The final directory structure looks like this:
-#		$ ls
-#		01/  02/  03/  04/  05/  06/  07/  08/  09/  10/  11/  12/
-#		$ ls 01  ## and all monthly directories look like this:
-#		Fri/  Mon/  Sat/  Sun/  Thu/  Tue/  Wed/
-#		
+# USAGE:	see usage string in the script
+# LOGIC:	Local backups:
+#		  A specified list of directories is each compressed into 
+#		  a 'tar' archive and stored on the local disk.  The script
+#		  cycles through the days of the week, overwriting older backups
+#		  in what is known as a circular buffer.
+#		  The script also starts a new top directory for a new month,
+#		  using the month number.  This effectively keeps the last
+#		  week of each month only. The buffer is larger, but not
+#		  overly big.
+#		  The final directory structure looks like this:
+#		  $ ls      ## (the months of a year by number)
+#		  01/  02/  03/  04/  05/  06/  07/  08/  09/  10/  11/  12/
+#
+#		  All monthly directories look like this:
+#		  $ ls 01
+#		  Fri/  Mon/  Sat/  Sun/  Thu/  Tue/  Wed/
+#
+#		External backups (to a removable device):
+#		  This is a simple local rsync of the contents of the 
+#		  local backups to the removable device
+#
+#		Large directory backups (to a removable device):
+#		  This is a simple local rsync of the specified large 
+#		  directories to a removable device.  The schedule of
+#		  the backups is determined by your configuration value
+#		  for 'rsync_days', as well as your cron job frequency.
+#
+#		Large directory synchronization (to a removable device):
+#		  This is an option that would typically be done from time
+#		  to time (to maintain a true extra copy of large directories).
 ###############################################################################
 #set -v		# debugging tools
 #set -x
@@ -33,20 +49,8 @@ PATH="/bin:/usr/bin"
 
 cmd=$(basename $0)
 
-function errexit() {
-  echo "$cmd: $1"
-  exit 1
-}
-
-conf="/etc/system-backup.conf"
-if [ ! -f $conf ] ; then
-  errexit "Missing configuration file '$conf'"
-fi
-
-. $conf
-
 debug="no"
-function dbg() {
+function f_dbg() {
   if [ "$debug" = "no" ] ; then
     return 1
   else
@@ -54,52 +58,115 @@ function dbg() {
   fi
 }
 
-function usage() {
-  D="output debug messages about what you would do"
-  Y="means really do local backups"
-  X="means write the backed up data to the external device"
-  usagestr="USAGE:\t$cmd [ -D ] [-Y] [ -X ]\nWHERE:\t-D\t-- $D\n\t-Y\t-- $Y\n\t-X\t-- $X"
-  echo -e $usagestr
+conf="/etc/system-backup.conf"
+if [ ! -f $conf ] ; then
+  echo "Missing configuration file '$conf'"
+  exit 1
+fi
+. $conf
+
+if [ ! -d "$logdir" ] ; then
+  echo "Missing logging directory '$logdir'"
+  exit 1
+fi
+permlogfile=$logdir"/backups.log"
+
+function f_errexit() {
+  date=$(date +"%b %d %H:%M:%S")
+  f_dbg && echo "$cmd: $1"
+  f_dbg || echo "$date $1" >> $permlogfile
   exit 1
 }
 
-## parse cmd-line args
-if [ $# -lt 1 ] ; then
-  usage
-fi
+usagestr="\
+USAGE:\t$cmd [--test] [--local] [ ---external ] [ --rsync-large ]
+WHERE:\t--test\t\t-- output debug messages about what you would do
+\t--local\t\t-- do local backups
+\t--external\t-- write today's backed up data to the external device
+\t--rsync-large\t-- rsync large files to the external device
+\t--rsync-copy\t-- on-demand update of latest rsync backup on external device"
 
-doit="no"
-external="no"
-while [ $1 ] ; do
-  case $1 in
-    '-D') debug="yes";;
-    '-Y') doit="yes";;
-    '-X') external="yes";;
-       *) usage;;
-  esac
-  shift
-done
+function f_usage() {
+  echo -e "$usagestr"
+  exit 1
+}
 
-dbg && echo "debug is $debug, backup is $doit, external copy is $external"
+local="no"
+extern="no"
+rsync="no"
+rcopy=""
+function f_getargs() {
+  ## parse cmd-line args
+  if [ $# -lt 1 ] ; then
+    f_usage
+  fi
+  while [ $1 ] ; do
+    case $1 in
+                --test) debug="yes";;
+               --local) local="yes";;
+            --external) extern="yes";;
+         --rsync-large) rsync="yes";;
+          --rsync-copy) shift
+                        if [ $1 ] ; then
+                          re='^[0-6]$'
+                          if [[ $1 =~ $re ]] ; then
+                            rcopy="$1"
+                          else
+                            f_errexit "'$1' must be a number from 0 through 6"
+                          fi
+                        else
+                          f_usage
+                        fi ;;
+                     *) f_usage;;
+    esac
+    shift
+  done
+}
+
+mounted="no"
+function f_prep_external() {
+  if [ "$mounted" = "yes" ] ; then
+    return;
+  fi
+  lsblk | grep -q "$usbpartition"
+  if [ $? -ne 0 ] ; then
+    f_errexit "Partition '/dev/$usbpartition' for external backups not seen"
+  else
+    grep -q "$mntpoint" /etc/mtab
+    if [ $? -eq 0 ] ; then
+      f_errexit "'$mntpoint' is already mounted - please check"
+    fi
+  fi
+  f_dbg && echo mount /dev/$usbpartition $mntpoint
+  mount /dev/$usbpartition $mntpoint
+  if [ $? -ne 0 ] ; then
+    f_errexit "Could not mount partition '/dev/$usbpartition'"
+  else
+    mounted="yes"
+  fi
+}
+
+f_getargs "$@"
 
 id=$(id -u)
 if [ $id -ne 0 ] ; then
-  errexit "You are not root"
+  f_errexit "You are not root"
 fi
 
 if [ ! -d "$localtarget" ] ; then
-  errexit "Missing backups directory '$localtarget'"
+  f_errexit "Missing backups directory '$localtarget'"
 else
   cd $localtarget
   if [ $? -ne 0 ] ; then
-    errexit "Cannot change directories to '$localtarget'"
+    f_errexit "Cannot change directories to '$localtarget'"
   fi
 fi
 
-if [ "$doit" = "yes" ] ; then
+tmplogfile="/tmp/local-backup_"$$
+if [ "$local" = "yes" ] ; then
   ## Let's check available disk space before doing backups
   typeset -i destsize
-  destsize=`df -k $localtarget | awk '{print $2}'|grep -v blocks`
+  destsize=`df -k $localtarget|awk '{print $2}'|grep -v blocks`
 
   typeset -i tmpsum
   typeset -i sum
@@ -108,20 +175,20 @@ if [ "$doit" = "yes" ] ; then
     if [ -d $dir ] ; then
       tmpsum=`du -sk $dir|awk '{print $1}'`
       sum=$sum+$tmpsum
-      dbg && echo -n " In $dir space used is: $tmpsum; "
-      dbg && echo " Total space required is now: $sum"
+      f_dbg && echo -n " In $dir space used is: $tmpsum; "
+      f_dbg && echo " Total space required is now: $sum"
     fi
   done
 
   ## Note that we will compress the backed-up files, so we will need
   ## less space then is assumed here; that is okay - don't pinch the filesystem
-  dbg && echo " Kbytes of space available: $destsize and space needed: $sum"
+  f_dbg && echo " Kbytes of space available: $destsize and space needed: $sum"
   sum=$sum+500000		## add a buffer of half a megabyte
-  dbg && echo " Required space with half a megabyte buffer is: $sum"
+  f_dbg && echo " Required space with half a megabyte buffer is: $sum"
   if [ $destsize -lt $sum ] ; then
-    errexit "There is not enough space to backup files in $localtarget"
+    f_errexit "There is not enough space to backup files in $localtarget"
   else
-    dbg && echo " We are okay for space to backup files in $localtarget"
+    f_dbg && echo " We are okay for space to backup files in $localtarget"
   fi
 
   today=$(date +%a)	## short form of the day of the week in your language
@@ -130,72 +197,133 @@ if [ "$doit" = "yes" ] ; then
   if [ ! -d "$folder" ] ; then
     mkdir -p $folder
     if [ $? -ne 0 ] ; then
-      errexit "Could not make directory to '$localtarget/$folder'"
+      f_errexit "Could not make directory '$localtarget/$folder'"
     fi
   fi
   cd $localtarget/$folder
   if [ $? -ne 0 ] ; then
-    errexit "Cannot change directories to '$localtarget/$folder'"
+    f_errexit "Cannot change directories to '$localtarget/$folder'"
   fi
  
   for dir in $sysdirs ; do
-    logfile="/tmp/local-backup_"$$
     if [ -d $dir ] ; then
       ## remove the leading slash, and replace subsequent ones with '_'
       name=$(echo $dir | sed 's%/%%' | sed 's%/%_%g')
-      if [ "$doit" = "yes" ] ; then
-        if [ "$debug" = "yes" ] ; then
-          echo tar --ignore-failed-read -zcf $name".tgz" $dir
-        else
-          tar --ignore-failed-read -zcf $name".tgz" $dir >>$logfile 2>&1
-        fi
-      fi
+      f_dbg && echo tar --ignore-failed-read -zcf $name".tgz" $dir
+
+      f_dbg || tar --ignore-failed-read -zcf $name".tgz" $dir >>$tmplogfile 2>&1
     fi
   done
-  if [ -e "$logfile" ] ; then  ## else we are in debug mode
-    output=$(grep -c -v "tar: Removing leading \`/' from member names" $logfile)
+  if [ -e "$tmplogfile" ] ; then  ## else we are in debug mode
+    output=$(grep -c -v "tar: Removing leading \`/' from member names" $tmplogfile)
     if [ "$output" != "0" ] ; then
+      cat  "$output" >> $permlogfile
       echo "$output"
     fi
-    rm -f $logfile 2>/dev/null
+    rm -f $tmplogfile 2>/dev/null
   fi
 fi
 
-if [ "$external" = "yes" ] ; then
-  lsblk | grep -q "$usbpartition"
-  if [ $? -ne 0 ] ; then
-    errexit "Partition '/dev/$usbpartition' for external backups not seen"
+if [ "$extern" = "yes" ] ; then
+  f_prep_external
+  if [ ! -d "$usb_local_dirs" ] ; then
+    umount $mntpoint
+    f_errexit "Missing directory '$usb_local_dirs'"
   else
-    grep -q "$mntpoint" /etc/mtab
-    if [ $? -eq 0 ] ; then
-      errexit "'$mntpoint' is already mounted - please check"
+    f_dbg && echo rsync -aux $localtarget/ $usb_local_dirs/
+    f_dbg || rsync -aux $localtarget/ $usb_local_dirs/ >>$tmplogfile 2>&1
+    res=$?
+    if [ -f "$tmplogfile" ] ; then
+      cat  "$tmplogfile" >> $permlogfile
+      rm -f $tmplogfile 2>/dev/null
     fi
-  fi
-  dbg && echo mount /dev/$usbpartition $mntpoint
-  mount /dev/$usbpartition $mntpoint
-  if [ $? -ne 0 ] ; then
-    errexit "Could not mount partition '/dev/$usbpartition'"
-  else
-    if [ ! -d "$usbtarget" ] ; then
+    if [ $res -ne 0 ] ; then
       umount $mntpoint
-      errexit "Missing directory '$usbtarget'"
-    else
-      dbg && echo rsync -aux $localtarget/ $usbtarget/
-      if [ "$external" = "yes" ] ; then
-        if [ "$debug" = "yes" ] ; then
-          echo rsync -aux $localtarget/ $usbtarget/
-        else
-          rsync -aux $localtarget/ $usbtarget/
+      f_errexit "rsync from '$localtarget' to '$usb_local_dirs' had a problem"
+    fi
+    f_dbg && echo umount $mntpoint
+    umount $mntpoint
+    mounted="no"
+  fi
+fi
+if [ "$rsync" = "yes" ] ; then
+  if [ "$rsync_days" = "" ] ; then
+    f_errexit "Missing value for 'rsync_days' - the days of the week for rsyncs"
+  fi
+  dayofweek=$(date +%w)
+  do_rsync="no"
+  for i in $rsync_days ; do
+    if [ "$i" = "$dayofweek" ] ; then
+      do_rsync="yes"
+      continue
+    fi
+  done
+  if [ "$do_rsync" = "no" ] ; then
+    f_dbg && echo We do not do rsync backups for large directories today
+    exit 0
+  fi
+  f_prep_external
+  if [ ! -d "$usb_large_dirs" ] ; then
+    umount $mntpoint
+    f_errexit "Missing directory '$usb_large_dirs'"
+  else
+    folder=$usb_large_dirs"/"$dayofweek
+    if [ ! -d "$folder" ] ; then
+      mkdir -p $folder
+      if [ $? -ne 0 ] ; then
+        f_errexit "Could not make directory '$folder'"
+      fi
+    fi
+    for dir in $largedirs ; do
+      if [ -d $dir ] ; then
+        ## remove the leading slash, and replace subsequent ones with '_'
+        name=$(echo $dir | sed 's%/%%' | sed 's%/%_%g')
+        f_dbg && echo nice -n 19 rsync -aux $dir/ $usb_large_dirs/$dayofweek/$name
+        f_dbg || nice -n 19 rsync -aux $dir/ $usb_large_dirs/$dayofweek/$name >>$tmplogfile 2>&1
+        res=$?
+        if [ -f "$tmplogfile" ] ; then
+          cat  "$tmplogfile" >> $permlogfile
+          rm -f $tmplogfile 2>/dev/null
         fi
-        if [ $? -ne 0 ] ; then
+        if [ $res -ne 0 ] ; then
           umount $mntpoint
-          errexit "Note - rsync from '$localtarget' to '$usbtarget' had a problem"
+          f_errexit "rsync from '$localtarget' to '$usb_large_dirs' had a problem"
         fi
       fi
-      dbg && echo umount $mntpoint
+    done
+  fi
+  f_dbg && echo umount $mntpoint
+  umount $mntpoint
+  mounted="no"
+fi
+if [ "$rcopy" != "" ] ; then
+  f_prep_external
+  if [ ! -d "$usb_large_copy" ] ; then
+    umount $mntpoint
+    f_errexit "Missing directory '$usb_large_copy'"
+  fi
+  if [ ! -d "$usb_large_dirs/$rcopy" ] ; then
+    umount $mntpoint
+    f_errexit "Missing directory '$usb_large_dirs/$rcopy'"
+  else
+    f_dbg && echo nice -n 19 rsync -aux --delete \
+      $usb_large_dirs/$rcopy/ $usb_large_copy/
+    f_dbg || nice -n 19 rsync -aux --delete \
+      $usb_large_dirs/$rcopy/ $usb_large_copy/ >>$tmplogfile 2>&1
+    res=$?
+    if [ -f "$tmplogfile" ] ; then
+      cat  "$tmplogfile" >> $permlogfile
+      rm -f $tmplogfile 2>/dev/null
+    fi
+    if [ $res -ne 0 ] ; then
       umount $mntpoint
+      f_errexit "rsync from" "\
+       '$usb_large_dirs/$rcopy/' to '$usb_large_copy/' had a problem"
     fi
   fi
+  f_dbg && echo umount $mntpoint
+  umount $mntpoint
+  mounted="no"
 fi
 
 exit 0
